0e749f4222dd464ed27baa0a41d055e607c2df50
alexsandroluz <alexsandroluz@lampp-it.com.br>
2022-04-06 16:17:21 -0300

    Adicionar arquivos de projeto.

---
+++ AtividadetTriangulo/Triangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadetTriangulo
+{
+    internal class Triangulo
+    {
+       public int l1, l2, l3;
+     
+        public Triangulo()
+        {
+
+        }
+
+        public Triangulo(int l1, int l2, int l3)
+        {
+            this.l1 = l1;
+            this.l2 = l2;
+            this.l3 = l3;
+        }
+
+    }
+}
